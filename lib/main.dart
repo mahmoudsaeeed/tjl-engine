46ed3ce4bf8d56749e06509ea56391278ebbac18
mahmoud <mahmoudsaeedm24@gmail.com>
2024-08-13 12:47:03 +0300

    add hive_box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+
+import 'package:bloc_learn/models/engine_model_adapter.dart';
 import 'package:bloc_learn/presentaion/views/home_view.dart';
+import 'package:bloc_learn/utils/constants.dart';
 import 'package:bloc_learn/utils/my_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Directory dir = await getApplicationDocumentsDirectory();
+  Hive.registerAdapter(EngineModelAdapter());
+
+  await Hive.openBox(knameBox , path: dir.path);
+
   runApp(const MyApp());
 }
 
---
+++ lib/utils/def.dart
@@ -0,0 +1,4 @@
+import 'package:bloc_learn/utils/constants.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+final Box box = Hive.box(knameBox);
